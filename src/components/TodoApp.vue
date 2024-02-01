acc6aaeeac21b4c5fbc94da57f7a02e0c00abcb4
Jeevika Yarlagadda <47218938+yarlagaddajeevika@users.noreply.github.com>
2024-02-01 12:20:41 -0800

    Create TodoApp.vue

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,42 @@
+<!-- TodoApp.vue -->
+<template>
+  <div>
+    <h1>Vue.js Todo App</h1>
+    
+    <div>
+      <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo">
+      <button @click="addTodo">Add</button>
+    </div>
+
+    <ul>
+      <TodoItem v-for="(todo, index) in todos" :key="index" :todo="todo" @remove="removeTodo(index)" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  },
+  components: {
+    TodoItem
+  }
+};
+</script>
